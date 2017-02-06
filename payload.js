ee7c1cd09ec19f41bd620bae4f2b577960045331
Aquib Master <aquibmaster@gmail.com>
2017-02-06 16:53:09 +1300

    Disable sheild when modifiers are pressed

--- payload.js
+++ payload.js
@@ -1,9 +1,34 @@
-console.info(`[SELECTION SHIELD] Overriding selection requests from ${window.location}`);
+(function() {
+    console.info(`[SELECTION SHIELD] Overriding selection requests from ${window.location}`);
 
-window.getSelection = function() {
-    return {};
-}
+    const originalSelectionFunction = window.getSelection;
+    let modifiersPressed = [];
 
-document.getSelection = function() {
-    return {};
-}
+    window.addEventListener('keydown', (e) => {
+        if(e.metaKey || e.ctrlKey) {
+            modifiersPressed = [e.which, ...modifiersPressed];
+        }
+    });
+
+    window.addEventListener('keyup', (e) => {
+        modifiersPressed = modifiersPressed.filter((key) => {
+            return key !== e.which;
+        });
+    });
+
+    window.getSelection = function() {
+        if(modifiersPressed.length) {
+            return originalSelectionFunction();
+        }
+
+        return {};
+    }
+
+    document.getSelection = function() {
+        if(modifiersPressed.length) {
+            return originalSelectionFunction();
+        }
+
+        return {};
+    }
+})();
