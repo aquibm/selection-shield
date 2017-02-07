31c72f4c18a04ed9855e6c2a58de4df588dfebb6
Aquib Master <aquibmaster@gmail.com>
2017-02-07 21:49:14 +1300

    Configure build script to create packages in a dist directory

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,21 @@
 const zip = require('bestzip');
+const mkdirp = require('mkdirp');
 const manifest = require('../src/manifest.json');
 
 const packageName = `selection-shield-${manifest.version}.zip`;
 
-zip(packageName, ['src/*'], (error) => {
+mkdirp('./dist', (error) => {
+    handleError(error);
+
+    zip(`./dist/${packageName}`, ['src/*'], (error) => {
+        handleError(error);
+        console.info(`[Build] ${packageName} built successfully.`);
+    });
+});
+
+function handleError(error) {
     if(error) {
         console.error(error);
         process.exit(1);
     }
-
-    console.info(`[Build] ${packageName} built successfully.`);
-})
+}
